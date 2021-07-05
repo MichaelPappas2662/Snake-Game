cc4c4aa375e192952effaa474d69528991ae72ec
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-05 20:12:06 +1000

    adding code to quit game

--- snake.py
+++ snake.py
@@ -6,7 +6,8 @@ pygame.display.set_caption('Snake game by Michael')
 game_over=False
 while not game_over:
     for event in pygame.event.get():
-        print(event)   #prints out all the actions that take place on the screen
+        if event.type==pygame.QUIT:
+            game_over=True
  
 pygame.quit()
 quit()
